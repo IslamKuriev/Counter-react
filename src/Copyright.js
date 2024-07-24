af8dc3df3a158a074dd9ec73f76eebf40a946505
Islam <islamwork15@gmail.com>
2024-07-24 22:47:37 +0300

    new update

--- src/Copyright.js
+++ src/Copyright.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Copyright = () => {
     return (
-        <p className='copyright'>ООО “интукод”, 2024г.</p>
+        <p className='copyright'>ООО “Интукод”, 2024г.</p>
     );
 };
 
